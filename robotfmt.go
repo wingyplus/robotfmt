d00a993e3bf0e96558ed27c4b9b466e7586530c7
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-08-23 21:43:37 +0700

    Robot Framework Formatter
    The command `robotfmt` print formatted code by given filename.
    By default this command print the result to standard output. When
    use option `-w` it write the result to filename.

---
+++ robotfmt.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"text/tabwriter"
+)
+
+var (
+	write = flag.Bool("w", false, "write result to file instead of stdout")
+)
+
+func readFile(filename string) string {
+	b, _ := ioutil.ReadFile(filename)
+	return string(b)
+}
+
+func format(w io.Writer, content string) {
+	tabWriter := tabwriter.NewWriter(w, 0, 4, 4, ' ', 0)
+	fmt.Fprint(tabWriter, content)
+	tabWriter.Flush()
+}
+
+func main() {
+	flag.Parse()
+
+	filename := flag.Args()[0]
+	content := readFile(filename)
+
+	re := regexp.MustCompile(" {2,}")
+	content = re.ReplaceAllString(content, "\t")
+
+	var buf bytes.Buffer
+	format(&buf, content)
+
+	if *write {
+		ioutil.WriteFile(filename, buf.Bytes(), 0644)
+	} else {
+		io.Copy(os.Stdout, &buf)
+	}
+
+}
